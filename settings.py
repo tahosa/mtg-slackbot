208e8e0835b8e67ab0ec51ab11b8b815f4c2bd0b
Cody S <eristhulhu@gmail.com>
2016-09-07 15:02:50 -0600

    Comments in the settings file

--- settings.py
+++ settings.py
@@ -1,18 +1,22 @@
 # Bot Name
 BOT_NAME = 'MTGCardBot'
 
-# Bot's unique ID (google it)
+# Bot's unique ID (see script)
 BOT_ID = 'U2660SL06'
 
 AT_BOT = '<@' + BOT_ID + '>'
 
+# How long to wait between checking slack
 SLEEP_TIME = 1
 
+# Max number of cards returned
 MAX_CARDS = 10
 
+# Ordered list that the logic in the bot can take
 PARAM_LIST = [ 'name', 'set', 'cost', 'cmc', 'colors', 'supertypes', \
 'type', 'subtypes', 'rarity', 'power', 'toughness' ]
 
+# The help text
 HELP_TEXT = "```To use this bot simply metion it and give it a name to search like 'elesh norn' or a word to search on.\n" \
 + "To perform an advanced search use the syntax @<MTGCardBot> name=\"<name>\" set=\"<set>\" etc. The allowed values are:" \
 + " name, set, cost, cmc, colors, supertypes, type, subtypes, rarity, power, toughness.```"
